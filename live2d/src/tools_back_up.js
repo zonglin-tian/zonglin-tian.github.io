1d531391bd34603a75b6713b628530d23dfbd506
zonglin-tian <zonglin_tian@163.com>
2024-04-30 08:50:07 +0800

    Site updated: 2024-04-30 08:50:03

--- live2d/src/tools_back_up.js
+++ live2d/src/tools_back_up.js
@@ -1,9 +1,9 @@
 import fa_comment from "@fortawesome/fontawesome-free/svgs/solid/comment.svg";
-// import fa_paper_plane from "@fortawesome/fontawesome-free/svgs/solid/paper-plane.svg";
+import fa_paper_plane from "@fortawesome/fontawesome-free/svgs/solid/paper-plane.svg";
 import fa_user_circle from "@fortawesome/fontawesome-free/svgs/solid/circle-user.svg";
 import fa_street_view from "@fortawesome/fontawesome-free/svgs/solid/street-view.svg";
-// import fa_camera_retro from "@fortawesome/fontawesome-free/svgs/solid/camera-retro.svg";
-// import fa_info_circle from "@fortawesome/fontawesome-free/svgs/solid/circle-info.svg";
+import fa_camera_retro from "@fortawesome/fontawesome-free/svgs/solid/camera-retro.svg";
+import fa_info_circle from "@fortawesome/fontawesome-free/svgs/solid/circle-info.svg";
 import fa_xmark from "@fortawesome/fontawesome-free/svgs/solid/xmark.svg";
 
 import showMessage from "./message.js";
@@ -26,19 +26,19 @@ const tools = {
         icon: fa_comment,
         callback: showHitokoto
     },
-    // "asteroids": {
-    //     icon: fa_paper_plane,
-    //     callback: () => {
-    //         if (window.Asteroids) {
-    //             if (!window.ASTEROIDSPLAYERS) window.ASTEROIDSPLAYERS = [];
-    //             window.ASTEROIDSPLAYERS.push(new Asteroids());
-    //         } else {
-    //             const script = document.createElement("script");
-    //             script.src = "https://fastly.jsdelivr.net/gh/stevenjoezhang/asteroids/asteroids.js";
-    //             document.head.appendChild(script);
-    //         }
-    //     }
-    // },
+    "asteroids": {
+        icon: fa_paper_plane,
+        callback: () => {
+            if (window.Asteroids) {
+                if (!window.ASTEROIDSPLAYERS) window.ASTEROIDSPLAYERS = [];
+                window.ASTEROIDSPLAYERS.push(new Asteroids());
+            } else {
+                const script = document.createElement("script");
+                script.src = "https://fastly.jsdelivr.net/gh/stevenjoezhang/asteroids/asteroids.js";
+                document.head.appendChild(script);
+            }
+        }
+    },
     "switch-model": {
         icon: fa_user_circle,
         callback: () => {}
@@ -47,20 +47,20 @@ const tools = {
         icon: fa_street_view,
         callback: () => {}
     },
-    // "photo": {
-    //     icon: fa_camera_retro,
-    //     callback: () => {
-    //         showMessage("照好了嘛，是不是很可爱呢？", 6000, 9);
-    //         Live2D.captureName = "photo.png";
-    //         Live2D.captureFrame = true;
-    //     }
-    // },
-    // "info": {
-    //     icon: fa_info_circle,
-    //     callback: () => {
-    //         open("https://github.com/stevenjoezhang/live2d-widget");
-    //     }
-    // },
+    "photo": {
+        icon: fa_camera_retro,
+        callback: () => {
+            showMessage("照好了嘛，是不是很可爱呢？", 6000, 9);
+            Live2D.captureName = "photo.png";
+            Live2D.captureFrame = true;
+        }
+    },
+    "info": {
+        icon: fa_info_circle,
+        callback: () => {
+            open("https://github.com/stevenjoezhang/live2d-widget");
+        }
+    },
     "quit": {
         icon: fa_xmark,
         callback: () => {
