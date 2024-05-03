ec8c1a7b69a56b1ced5006ae4f5ec6cff2eccb2c
zonglin-tian <zonglin_tian@163.com>
2024-05-03 17:54:58 +0800

    Site updated: 2024-05-03 17:54:53

--- statistics/pycharts_data.py
+++ statistics/pycharts_data.py
@@ -1,7 +1,6 @@
-from pyecharts import options as opts
-from pyecharts.charts import Calendar
 import pandas as pd
 import numpy as np
+import json
 import ipdb
 
 data = pd.read_excel(io="T:/root/notes/geek_road/source/statistics/2024_data.xlsx")
@@ -9,17 +8,9 @@ data = pd.read_excel(io="T:/root/notes/geek_road/source/statistics/2024_data.xls
 data['date'] = data['date'].astype(str)
 
 data = np.array(data.iloc[:, 0:2])
-data = data.tolist()
-# print(data)
+shape = data.shape
+data = data.ravel()
+dict_data = {'data': data.tolist(), 'shape': shape}
 
-calendar_heatmap = (
-    Calendar()
-    .add("", data, calendar_opts=opts.CalendarOpts(range_=['2024-01-01', '2024-12-31'], pos_top="120"))
-    .set_global_opts(
-        title_opts=opts.TitleOpts(title="2024年热力图日历", pos_top="30", pos_left="center"),
-        visualmap_opts=opts.VisualMapOpts(
-            max_=500, min_=100, orient="horizontal", is_piecewise=False, pos_top="50", pos_left="center"
-        ),
-    )
-)
-calendar_heatmap.render("T:/root/notes/geek_road/source/statistics/get_up_echart.html")
+with open("T:/root/notes/geek_road/source/statistics/get_up_echart.json", 'w') as f:
+    json.dump(dict_data, f, indent=2)
