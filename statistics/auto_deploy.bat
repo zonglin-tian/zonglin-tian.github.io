e46e326983ab20b0a1ef43d664a4f2c3ccc3d481
zonglin-tian <zonglin_tian@163.com>
2024-06-22 09:57:41 +0800

    Site updated: 2024-06-22 09:57:32

--- statistics/auto_deploy.bat
+++ statistics/auto_deploy.bat
@@ -1,5 +1,7 @@
 @echo off
+REM 使用 UTF-8 编码保存日志文件
 chcp 65001 > nul
+
 REM 设置 hexo 博客路径
 set BLOG_DIR=T:\root\notes\geek_road
 REM 设置日志文件路径
@@ -8,11 +10,22 @@ set LOG_FILE=%BLOG_DIR%\source\statistics\ad_log.txt
 REM 进入 Hexo 项目目录
 cd /d %BLOG_DIR%
 
+REM 获取系统日期和时间
+set currentDate=%date%
+set currentTime=%time%
+REM 格式调整
+
+REM 切割时间字符串, 使用空格作为分隔符, 获取第一部分(即时间部分)
+for /F "tokens=1 delims= " %%A in ("%currentTime%") do set timePart=%%A
+
+REM 使用冒号作为分隔符, 获取时和分
+for /F "tokens=1,2 delims=:" %%B in ("%timePart%") do set timeCustom=%%B
+
 hexo clean && hexo generate && hexo deploy && if %errorlevel% neq 0 (
     echo Failed to deploy to GitHub Pages
     pause
     exit /b %errorlevel%
 ) else (
-    echo BLOG 最近部署时间: %date% %time% > %LOG_FILE%
+    echo BLOG 最近部署时间: %currentDate%/%timeCustom% > %LOG_FILE%
 )
 
