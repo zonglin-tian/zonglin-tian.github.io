5824f77f42f9e32a46ddd8d61bddc9da56bc0cd0
zonglin-tian <zonglin_tian@163.com>
2024-06-22 09:11:43 +0800

    Site updated: 2024-06-22 09:11:34

--- statistics/auto_deploy.bat
+++ statistics/auto_deploy.bat
@@ -5,14 +5,7 @@ set BLOG_DIR=T:\root\notes\geek_road
 REM 进入 Hexo 项目目录
 cd /d %BLOG_DIR%
 
-REM 清理之前生成的文件
-REM hexo clean
-
-REM 生成静态文件
-hexo generate
-
-REM 部署到 GitHub Pages
-hexo deploy
+hexo clean --silent && hexo generate --silent && hexo deploy
 
 REM 提示完成
 echo Deployment complete!
