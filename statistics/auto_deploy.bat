80310883c78936954feff48224f55a038ee74aae
zonglin-tian <zonglin_tian@163.com>
2024-06-22 22:03:51 +0800

    Site updated: 2024-06-22 22:03:41

--- statistics/auto_deploy.bat
+++ statistics/auto_deploy.bat
@@ -1,4 +1,5 @@
 @echo off
+
 REM 使用 UTF-8 编码保存日志文件
 REM chcp 65001 > nul
 
@@ -10,6 +11,17 @@ set LOG_FILE=%BLOG_DIR%\source\statistics\ad_log.txt
 REM 进入 Hexo 项目目录
 cd /d %BLOG_DIR%
 
+REM 开启延迟变量扩展
+setlocal enabledelayedexpansion
+REM 初始化
+set "lastUpdateTime="
+
+REM 读取文件内容到变量
+for /f "delims=" %%i in (%LOG_FILE%) do (
+    set "lastUpdateTime=!lastUpdateTime!%%i"
+)
+endlocal
+
 REM 获取系统日期和时间
 set currentDate=%date%
 set currentTime=%time%
@@ -18,12 +30,11 @@ REM 格式调整
 REM 使用 ,. 作为分隔符, 获取时分秒
 for /F "tokens=1 delims=,. " %%A in ("%currentTime%") do set timePart=%%A
 
+echo %currentDate%/%timePart% > %LOG_FILE%
+
 
 hexo clean --silent && hexo generate --silent && hexo deploy && if %errorlevel% neq 0 (
+    echo %lastUpdateTime% > %LOG_FILE%
     echo Failed to deploy to GitHub Pages
-    REM pause
     exit /b %errorlevel%
-) else (
-    echo %currentDate%/%timePart% > %LOG_FILE%
 )
-
