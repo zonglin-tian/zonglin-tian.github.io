b847e5ef625cde5fa764be6bee1fb912ce4fefab
zonglin-tian <zonglin_tian@163.com>
2024-08-08 17:55:07 +0800

    Site updated: 2024-08-08 17:54:59

--- statistics/auto_deploy.bat
+++ statistics/auto_deploy.bat
@@ -1,7 +1,7 @@
 @echo off
 
 REM 使用 UTF-8 编码保存日志文件
-REM chcp 65001 > nul
+chcp 65001 > nul
 
 REM 设置 hexo 博客路径
 set BLOG_DIR=T:\root\notes\geek_road
@@ -20,18 +20,21 @@ REM 读取文件内容到变量
 for /f "delims=" %%i in (%LOG_FILE%) do (
     set "lastUpdateTime=!lastUpdateTime!%%i"
 )
-endlocal
 
 REM 获取系统日期和时间
 set currentDate=%date%
 set currentTime=%time%
 REM 格式调整
 
+REM 使用 ,. 作为分隔符, 获取日期
+for /F "tokens=2 delims=,. " %%A in ("%currentDate%") do set datePart=%%A
+
 REM 使用 ,. 作为分隔符, 获取时分秒
 for /F "tokens=1 delims=,. " %%A in ("%currentTime%") do set timePart=%%A
 
-echo %currentDate%/%timePart% > %LOG_FILE%
+echo %datePart%/%timePart% > %LOG_FILE%
 
+endlocal
 
 hexo clean --silent && hexo generate --silent && hexo deploy && if %errorlevel% neq 0 (
     echo %lastUpdateTime% > %LOG_FILE%
