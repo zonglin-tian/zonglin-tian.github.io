45b13841b1e99dc7c6206bbf6dd22ff9ae01e2f9
zonglin-tian <zonglin_tian@163.com>
2025-06-04 14:59:48 +0800

    Site updated: 2025-06-04 14:59:37

--- js/address_bar_search.js
+++ js/address_bar_search.js
@@ -3,8 +3,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const searchQuery = urlParams.get('q');
     if (searchQuery) {
         const searchModal = new bootstrap.Modal(document.getElementById('modalSearch'));
+         // 在模态框显示的回调中执行填充和触发事件
+        searchModal._config.backdrop = 'false'; // 防止背景点击关闭模态框（可选）
         searchModal.show();
-        const searchInput = document.getElementById('local - search - input');
+        const searchInput = document.getElementById('local-search-input');
         if (searchInput) {
             searchInput.value = searchQuery;
             const inputEvent = new Event('input', { bubbles: true });
