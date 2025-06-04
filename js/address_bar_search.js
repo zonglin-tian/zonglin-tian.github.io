457799b68c2586a48881e6e06b7f9912dda477e6
zonglin-tian <zonglin_tian@163.com>
2025-06-04 15:04:59 +0800

    Site updated: 2025-06-04 15:04:44

--- js/address_bar_search.js
+++ js/address_bar_search.js
@@ -2,15 +2,21 @@ document.addEventListener('DOMContentLoaded', function () {
     const urlParams = new URLSearchParams(window.location.search);
     const searchQuery = urlParams.get('q');
     if (searchQuery) {
-        const searchModal = new bootstrap.Modal(document.getElementById('modalSearch'));
-         // 在模态框显示的回调中执行填充和触发事件
-        searchModal._config.backdrop = 'false'; // 防止背景点击关闭模态框（可选）
-        searchModal.show();
-        const searchInput = document.getElementById('local-search-input');
-        if (searchInput) {
-            searchInput.value = searchQuery;
-            const inputEvent = new Event('input', { bubbles: true });
-            searchInput.dispatchEvent(inputEvent);
-        }
+        // 重定向到首页
+        window.location.href = '/';
+        // 使用 setTimeout 确保重定向后代码执行（因为重定向是异步的）
+        setTimeout(function () {
+            const searchModal = new bootstrap.Modal(document.getElementById('modalSearch'));
+            searchModal._config.backdrop = 'false'; // 防止背景点击关闭模态框（可选）
+            searchModal.show();
+            searchModal._element.addEventListener('shown.bs.modal', function () {
+                const searchInput = document.getElementById('local-search-input');
+                if (searchInput) {
+                    searchInput.value = searchQuery;
+                    const inputEvent = new Event('input', { bubbles: true });
+                    searchInput.dispatchEvent(inputEvent);
+                }
+            });
+        }, 100); // 100 毫秒的延迟，可根据实际情况调整
     }
 });
