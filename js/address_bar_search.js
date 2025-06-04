9703c555594fba31dafd6ed305a58c2a443307bc
zonglin-tian <zonglin_tian@163.com>
2025-06-04 16:04:22 +0800

    Site updated: 2025-06-04 16:04:05

--- js/address_bar_search.js
+++ js/address_bar_search.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const urlParams = new URLSearchParams(window.location.search);
     const searchQuery = urlParams.get('q');
     if (searchQuery) {
+        // 重定向到首页
+        window.location.href = '/';
         // 使用 setTimeout 确保重定向后代码执行（因为重定向是异步的）
         setTimeout(function () {
-            // 重定向到首页
-            window.location.href = '/';
             const searchModal = new bootstrap.Modal(document.getElementById('modalSearch'));
             searchModal._config.backdrop = 'false'; // 防止背景点击关闭模态框（可选）
             searchModal.show();
